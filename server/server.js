f6928f0d6b569de212b07d56a80b5416376ecfb7
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-03-09 10:01:28 -0500

    attempting step 4 of advanced

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let path = require('path');
-// let bodyParser = require('body-parser')
+let bodyParser = require('body-parser')
 let fs = require('fs');
 // let router = express.router
 let app = express();
@@ -10,33 +10,38 @@ let app = express();
 //     res.send("Hello from the web server side")
 // })
 
-app.use((req, res, next) =>{
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use((req, res, next) => {
     console.log(req.originalUrl);
     next();
-})
+});
 
 
 // The below is the attempts at the 4th step of the advanced portion of the lab:
 
-    // app.use((req, res, next) => {
-    //     fs.appendFile('log.txt', `${req.url}\n`);
-    //     next()
-    // })
+// app.use((req, res, next) => {
+//     fs.appendFile('log.txt', `${req.url}\n`);
+//     next()
+// })
 
 // let dataPath = path.join(__dirname, "../formsubmissions.json")
 
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.post('/contact-form', (req, res) => {
-//     fs.appendFileSync('log.txt', `Name: ${req.body.name}, Email: ${req.body.email}\n`);
-//     res.send('/formsubmission');
-// })
-// // app.get form submit???
+app.post('/contact-form', (req, res) => {
+    let response = {
+        name: req.body.name,
+        email: req.body.email
+    }
+    fs.appendFileSync('log.json', JSON.stringify(response));
+    res.redirect('/');
+});
 
-// router.get('/formsubmission', (req, res)=>{
+app.get('/formsubmissions', (req, res) => {
+    let data = fs.readFileSync('./log.json');
+    res.sendFile(data);
+});
 
-// })
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use(express.static(path.join(__dirname, '../public')))
-
-
-app.listen(3000)
+app.listen(3000);
